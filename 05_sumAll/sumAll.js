903d8c9906773fd66f97133271edcdb5ad0ec463
iampatel3372 <himanshukumarpatel3372@gmail.com>
2022-07-28 16:33:05 +0530

    Made Reqiured Changes

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    if(num1 < 0 || num2 < 0 || typeof(num1) !=  "number" || typeof(num2) != "number") return "ERROR";
+    let res = 0;
+    if(num1 > num2) [num1, num2] = [num2, num1];
+    while(num1 <= num2) {
+        res +=  num1;
+        num1++;
+    } 
+    return res;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,12 @@
-const ftoc = function() {
+const ftoc = function(temp) {
+    let res = (temp - 32) / 1.8;
 
+    return Number(res.toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(temp) {
+    let res = temp * 1.8 + 32;
+    return Number(res.toFixed(1)); 
 };
 
 // Do not edit below this line
